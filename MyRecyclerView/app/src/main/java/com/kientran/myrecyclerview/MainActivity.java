685d947c0ef2abdaaad6ebcbf6b35a074bc48862
Kien Tran <n01260692@humbermail.ca>
2019-11-05 20:38:07 -0500

    RecyclerView

--- MyRecyclerView/app/src/main/java/com/kientran/myrecyclerview/MainActivity.java
+++ MyRecyclerView/app/src/main/java/com/kientran/myrecyclerview/MainActivity.java
@@ -3,12 +3,21 @@ package com.kientran.myrecyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.kientran.myrecyclerview.dummy.DummyContent;
+
+public class MainActivity extends AppCompatActivity implements LaptopFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+        Toast.makeText(this, "Clicked : " + item.toString() , Toast.LENGTH_SHORT).show();
     }
 }
